fb09ff5792f798bf2fb24ed8ab3badece30e5f45
jeff(황용대) <jeff.dean@kakaomobility.com>
2018-05-08 15:22:18 +0900

    정리 끝

--- _assets/javascripts/application.js
+++ _assets/javascripts/application.js
@@ -1,12 +1,5 @@
 //= require_self
 
 $(function() {
-  $('#fullpage').fullpage({
-    verticalCentered: false,
-    anchors: anchors,
-    menu: '#menu',
-    navigation: true,
-    controlArrows: false,
-    'css3': true
-  });
+  
 });
--- _assets/javascripts/vendor.js
+++ _assets/javascripts/vendor.js
@@ -1,3 +1,11 @@
+//= require tether/dist/js/tether
 //= require bootstrap/dist/js/bootstrap
-//= require fullpage.js/dist/jquery.fullpage
-//= require fullpage.js/dist/jquery.fullpage.extensions.min
+//= require smoothscroll/smoothscroll
+//= require jquery-dropdown/jquery-dropdown
+//= require jquery-touchswipe/jquery.touchSwipe
+//= require jarallax/dist/jarallax
+//= require countdown/jquery.countdown.min
+//= require jquery.mb.ytplayer/dist/jquery.mb.YTPlayer
+//= require jquery.mb.vimeo_player/dist/jquery.mb.vimeo_player
+//= require social-likes/dist/social-likes.min
+//= require bootstrap-carousel-swipe/carousel-swipe
