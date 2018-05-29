2d9ae4a0ca68304002897796961c1f628673ecb6
jeff(황용대) <jeff.dean@kakaomobility.com>
2018-05-30 01:33:05 +0900

    비디오를 열심히 조정했으나...

--- _assets/javascripts/application.js
+++ _assets/javascripts/application.js
@@ -1,5 +1,8 @@
 //= require_self
 
 $(function() {
-
+  $(".mbr-background-video").on("VPReady", function(e) {
+    $('.mbr-background-video-preview').hide();
+    $('.mbr-background-video').v_play();
+  });
 });
